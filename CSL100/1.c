5c9db475a3c7f1a7fadfa9ced0fd56cc3d49ae3e
Ashutosh <prajapatiashu777@gmail.com>
2024-08-07 00:12:23 +0530

    init

--- CSL100/1.c
+++ CSL100/1.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 int main(){
-    printf("Hello world!");
+    char name[30];
+    char roll[20];
+    printf("Enter name:");
+    scanf("%s",&name);
+    printf("Enter roll: ");
+    scanf("%s",&roll);
+    printf("%s \n",name);
+    printf("%s",roll);
+
     return 0;
 }
