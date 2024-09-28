63c87cb45314a13382cbc88a090583746b4359f2
Ashutosh <prajapatiashu777@gmail.com>
2024-09-28 07:47:45 +0530

    added hcf by recursion

--- learning/lcmhcf.c
+++ learning/lcmhcf.c
@@ -29,13 +29,33 @@ int lcm(int a, int b){
     return lcm;
 }
 
+int hcf_rec(int a, int b){
+    int max,min;
+    if(a>=b){
+        max = a;
+        min = b;
+    }
+    else{
+        max = b;
+        min = a;
+    }
+    if((max % min)==0){
+        return min;
+    }
+    else{
+        int hcf = hcf_rec(max,max%min);
+    }
+}
 int main(){
     int a,b;
     printf("Enter number: ");
     scanf("%d %d",&a,&b);
     int HCF = hcf(a,b);
-    printf("HCF is %d",HCF);
+    printf("HCF is %d. \n",HCF);
     int LCM = lcm(a,b);
-    printf("LCM is %d",LCM);
+    printf("LCM is %d. \n",LCM);
+
+    int hcfrec = hcf_rec(a,b);
+    printf("HCF by recursion is %d.",hcfrec);
     return 0;
 }
