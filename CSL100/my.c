1497af60691159c9960861241319d65473ba7c60
Ashutosh <prajapatiashu777@gmail.com>
2024-09-29 19:37:41 +0530

    test

--- CSL100/my.c
+++ CSL100/my.c
@@ -1,5 +1,39 @@
-#include<stdio.h>
-int main(){
-    printf("Ashutosh Prajapati");
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // int n,i=4;
+    // long long int j = -4;
+    
+    // printf("%d \n",n);
+    // long long int j = (n<0? 0:n*n);
+    // printf("%lld",j);
+    // int ji = 65 ;
+    // printf ( "\nji >= 65 ? %d : %c", ji ) ;
+//     int i = 2, j = 3, k, l ; 
+// float a, b ; 
+// k = i / j * j ; 
+// l = j / i * i ; 
+// a = i / j * j ; 
+// b = j / i * i ; 
+// printf( "%d %d %f %f", k, l, a, b ) ;
+//     int a, b ; 
+//     int c =6;
+//     printf("%d",- -3);
+// a = -3 - - 3 ; 
+// b = -3 - - ( - 3 ) ; 
+// printf ( "a = %d b = %d", a, b ) ;
+//  int j,i=-4 ; 
+//  printf("%d",j);
+//  while ( j <= 10 ) 
+//  printf ( "%d", j ) ;  
+// char c ;
+// printf("%d",c);
+// int x = 4;
+// while(x>1)
+//     x= x -1;
+//  printf("%d",x);
+  char x[5] = "aedas";
+    printf("%s",x);
     return 0;
 }
